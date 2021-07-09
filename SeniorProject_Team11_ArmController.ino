fbe25b41c343678f895c7b42f5d74ded2377f1e0
Philip Kaczanowski <56287652+ClownBaby54@users.noreply.github.com>
2021-07-09 10:43:17 -0400

    Update SeniorProject_Team11_ArmController.ino
    Transfer for arduino mega

--- SeniorProject_Team11_ArmController.ino
+++ SeniorProject_Team11_ArmController.ino
@@ -12,10 +12,10 @@
 
 // instantiate an object for the nRF24L01 transceiver
 RF24 radio(7, 8); // using pin 7 for the CE pin, and pin 8 for the CSN pin
-//Pin 13 -> SCK
-//Pin 12 -> MISO
-//Pin 11 -> MOSI
-//Pin  8 -> CSN
+//Pin 13 -> SCK  (52)
+//Pin 12 -> MISO (50)
+//Pin 11 -> MOSI (51)
+//Pin  8 -> CSN  
 //Pin  7 -> CE
 //Unused -> IRQ
 
@@ -29,11 +29,7 @@ const int servoPin3 = 5; // the digital pin used for the first servo
 
 // an identifying device destination
 // Let these addresses be used for the pair
-//uint8_t address[][6] = {"10dar", "20dar"};
 uint8_t address[][6] = {"10dar", "30dar"};
-// It is very helpful to think of an address as a path instead of as
-// an identifying device destination
-// to use different addresses on a pair of radios, we need a variable to
 
 // uniquely identify which address this radio will use to transmit
 bool radioNumber = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
@@ -43,10 +39,6 @@ bool role = false;  // true = TX role, false = RX role
 
 int letssee = 10;
 
-// For this example, we'll be using a payload containing
-// a string & an integer number that will be incremented
-// on every successful transmission.
-// Make a data structure to store the entire payload of different datatypes
 struct PayloadStruct {
   char message[13];          // only using 13 characters for TX & ACK payloads
   uint8_t counter;
@@ -56,7 +48,7 @@ PayloadStruct payload;
 void setup() {
 
   
-//  Serial.begin(115200);
+  Serial.begin(115200);
   myservo1.attach(servoPin1);  // attaches the servo on pin 9 to the servo object
   myservo1.write(0,20,true); // set the intial position of the servo, as fast as possible, run in background
   myservo2.attach(servoPin2);  // attaches the servo on pin 9 to the servo object
@@ -64,21 +56,24 @@ void setup() {
   myservo3.attach(servoPin3);  // attaches the servo on pin 9 to the servo object
   myservo3.write(0,20,true);  // set the intial position of the servo, as fast as possible, wait until done
 
+//ensure that the select is turned on to enable commmunication
+  pinMode(53, OUTPUT);
+  digitalWrite(53, HIGH);
+
     // initialize the transceiver on the SPI bus
   if (!radio.begin()) {
     while (1) {} // hold in infinite loop
   }
+  
+  pinMode(53, OUTPUT);
+  digitalWrite(53, HIGH);
+  Serial.println("Connected");
   // Set the PA Level low to try preventing power supply related problems
   // because these examples are likely run with nodes in close proximity to
   // each other.
   radio.setPALevel(RF24_PA_LOW);     // RF24_PA_MAX is default.
 
-  // to use ACK payloads, we need to enable dynamic payload lengths (for all nodes)
   radio.enableDynamicPayloads();    // ACK payloads are dynamically sized
-
-  // Acknowledgement packets have no payloads by default. We need to enable
-  // this feature for all nodes (TX & RX) to use ACK payloads.
-//  radio.enableAckPayload();
 
   // set the TX address of the RX node into the TX pipe
   radio.openWritingPipe(address[radioNumber]);     // always uses pipe 0
@@ -86,36 +81,24 @@ void setup() {
   // set the RX address of the TX node into a RX pipe
   radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1
 
-  // setup the ACK payload & load the first response into the FIFO
-//  memcpy(payload.message, "12 ", 3);                       // set the payload message
-//  // load the payload for the first received transmission on pipe 0
-//  radio.writeAckPayload(1, &payload, sizeof(PayloadStruct));
-
   radio.startListening();                                     // put radio in RX mode
 }
 
 void loop() 
 {
+  
     int Servo1Angle;
     int Servo2Angle;
     int Servo3Angle;
     
     uint8_t pipe;
- 
+
     if (radio.available(&pipe)) 
     {                    // is there a payload? get the pipe number that recieved it
       uint8_t bytes = radio.getDynamicPayloadSize(); // get the size of the payload
       PayloadStruct received;
       radio.read(&received, sizeof(received));       // get incoming payload
 
-      // save incoming counter & increment for next outgoing
-//      payload.counter = received.counter + 1;
-//      letssee = letssee+1;
-//      sprintf(payload.message, "%d", letssee);
-//      // load the payload for the first received transmission on pipe 0
-//      radio.writeAckPayload(1, &payload, sizeof(payload));
-      //need to send data to dongle in order to get it received.
-//      memcpy(payload.message, "Hello ", 6);                       // set the payload message
       radio.stopListening();                                      // put radio in TX mode
       radio.write(&received, sizeof(received));    // transmit to usb dongle for data display. We dont care if it doesnt receieve it since we'll see it
       radio.startListening();
@@ -148,12 +131,9 @@ void loop()
 //        Servo3Angle = (float)((received.message[4] - '0')*10 + (received.message[5] - '0'))/99*180;
 //      }
       
-//      Serial.println(received.message[0]);
+      Serial.println(received.message[0]);
 //      Serial.println(Servo1Angle);
       
-//      Servo1Angle = ((received.message[0] - '0')*10 + (received.message[1] - '0'))/99*180;
-//      Servo2Angle = ((received.message[1] - '0')*10 + (received.message[2] - '0'))/99*180;
-//      Servo3Angle = ((received.message[3] - '0')*10 + (received.message[4] - '0'))/99*180;
       
       myservo1.write(Servo1Angle,50,false); // set the position of the servo, as fast as possible, run in background
 //      myservo2.write(Servo2Angle,50,false); // set the position of the servo, as fast as possible, run in background
